cdc73f85181057b8ca2fe25ac899fcf8a3537384
Brajan231 <101121594+Brajan231@users.noreply.github.com>
2022-03-07 15:36:20 +0000

    Update game.py
    made a moving circle in pygame

--- game.py
+++ game.py
@@ -1,3 +1,55 @@
-Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
+
+#------------------------
+#import library
+#------------------------
+import pygame
+
+#-----------------------
+#initialisation
+
+#-----------------------
+pygame.init()
+
+screen = pygame.display.set_mode([500,500])
+circle_pos = (250, 250)
+circle_pos2 = (150, 150)
+#variables
+#----------------------
+running = True
+#----------------------
+
+#-----------------------
+#loop
+#-----------------------
+while running:
+    #-------------------
+    #input
+    #-------------------
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                circle_pos = (circle_pos[0]-10,circle_pos[1])
+            if event.key == pygame.K_RIGHT:
+                circle_pos = (circle_pos[0]+10,circle_pos[1])
+            if event.key == pygame.K_UP:
+                circle_pos = (circle_pos[0],circle_pos[1]-10)
+            if event.key == pygame.K_DOWN:
+                circle_pos = (circle_pos[0],circle_pos[1]+10)
+                
+                
+    #------------------
+    #update
+    #------------------
+    
+    #Draw
+    #Fill background
+    #-------------------
+    screen.fill((25, 245, 255))
+    pygame.draw.circle(screen, (20, 255, 180), (circle_pos), 75)
+    pygame.display.flip()
+            
+pygame.quit()
+
+
